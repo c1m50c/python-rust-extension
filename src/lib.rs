03d02e7ea738f896393c669f9d3ebdcb17ae9142
c1m50c <terminalityms@gmail.com>
2021-12-12 14:45:27 -0600

    Function Cleanup 🧹

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 extern crate cpython;
 
 
-use cpython::{Python, PyResult, py_module_initializer, py_fn};
+use cpython::{Python, py_module_initializer, py_fn};
+use cpython::{PyResult, PyList, PyString, PythonObject, ToPyObject};
 
 
 py_module_initializer!(pyrse, |py, m| {
@@ -12,13 +13,20 @@ py_module_initializer!(pyrse, |py, m| {
 });
 
 
-fn pyrse_value(_py: Python, value: &str) -> PyResult<String> {
-    return Ok(format!("PyRsE: {}", value).to_owned());
+fn pyrse_value(_py: Python, value: &str) -> PyResult<PyString> {
+    return Ok(PyString::new(_py, format!("PyRsE: {}", value).as_ref()));
 }
 
 
-fn pyrse_count(_py: Python, to: i64) -> PyResult<String> {
-    let mut result: String = String::new();
-    for i in 0 .. to { result.push_str(&i.to_string()); }
+fn pyrse_count(_py: Python, to: i64) -> PyResult<PyList> {
+    let result: PyList = PyList::new(_py, &[  ]);
+
+    for i in 0 .. to {
+        result.append(
+            _py,
+            i.to_py_object(_py).into_object()
+        );
+    }
+    
     return Ok(result);
 }
